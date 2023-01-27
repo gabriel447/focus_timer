604ab1f227d4849e18f0e5cf8aa2cd178f9406e7
unknown <fullstackgabe@gmail.com>
2023-01-26 23:05:54 -0300

    exemplo de modularizaçãoi

--- js/index.js
+++ js/index.js
@@ -7,6 +7,9 @@
 // Recursividade = é quando uma função chama ela mesma.
 // cleancode = nomes significativos
 
+import resetControls from "./controls";
+import { countdown, resetTimer, updateTimerDisplay } from "./timer";
+
 const buttonPlay = document.querySelector(".play");
 const buttonPause = document.querySelector(".pause");
 const buttonStop = document.querySelector(".stop");
@@ -18,45 +21,6 @@ const secondsDisplay = document.querySelector(".seconds");
 let minutes = Number(minutesDisplay.textContent);
 let timerTimeOut;
 
-function resetControls() {
-  buttonPlay.classList.remove("hide");
-  buttonPause.classList.add("hide");
-  buttonSet.classList.remove("hide");
-  buttonStop.classList.add("hide");
-}
-
-function updateTimerDisplay(minutes, seconds) {
-  minutesDisplay.textContent = String(minutes).padStart(2, "0");
-  secondsDisplay.textContent = String(seconds).padStart(2, "0");
-}
-
-function resetTimer() {
-  updateTimerDisplay(minutes, 0);
-  clearTimeout(timerTimeOut);
-}
-
-function countdown() {
-  timerTimeOut = setTimeout(function () {
-    let seconds = Number(secondsDisplay.textContent);
-    let minutes = Number(minutesDisplay.textContent);
-
-    updateTimerDisplay(minutes, 0);
-
-    if (minutes <= 0) {
-      resetControls();
-      return;
-    }
-
-    if (seconds <= 0) {
-      seconds = 2;
-      --minutes;
-    }
-
-    updateTimerDisplay(minutes, String(seconds - 1));
-
-    countdown();
-  }, 1000);
-}
 
 buttonPlay.addEventListener("click", function () {
   buttonPlay.classList.add("hide");
@@ -90,9 +54,9 @@ buttonSoundOff.addEventListener("click", function () {
 
 buttonSet.addEventListener("click", function () {
   let newMinutes = prompt("Quantos minutos ?");
-  if(!newMinutes) {
+  if (!newMinutes) {
     resetTimer();
-    return
+    return;
   }
 
   minutes = newMinutes;
