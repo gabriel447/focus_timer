46303ebb8b0d4e9bf3fc6dab26f39418e4082f3a
unknown <fullstackgabe@gmail.com>
2023-01-27 17:26:00 -0300

    Resolvendo bugs

--- js/timer.js
+++ js/timer.js
@@ -3,13 +3,13 @@ export default function Timer({
   secondsDisplay,
   resetControls,
 }) {
-
   let timerTimeOut;
   let minutes = Number(minutesDisplay.textContent);
 
-
-  function updateDisplay(minutes, seconds) {
-    minutesDisplay.textContent = String(minutes).padStart(2, "0");
+  function updateDisplay(newMinutes, seconds) {
+    newMinutes = newMinutes === undefined ? minutes : newMinutes;
+    seconds = seconds === undefined ? 0 : seconds;
+    minutesDisplay.textContent = String(newMinutes).padStart(2, "0");
     secondsDisplay.textContent = String(seconds).padStart(2, "0");
   }
 
@@ -22,16 +22,18 @@ export default function Timer({
     timerTimeOut = setTimeout(function () {
       let seconds = Number(secondsDisplay.textContent);
       let minutes = Number(minutesDisplay.textContent);
+      let isFinished = minutes <= 0 && seconds <= 0;
 
       updateDisplay(minutes, 0);
 
-      if (minutes <= 0) {
+      if (isFinished) {
         resetControls();
+        updateDisplay();
         return;
       }
 
       if (seconds <= 0) {
-        seconds = 2;
+        seconds = 60;
         --minutes;
       }
 
@@ -54,6 +56,6 @@ export default function Timer({
     reset,
     updateDisplay,
     updateMinutes,
-    hold
+    hold,
   };
 }
