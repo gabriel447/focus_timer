d85359bc851bfe83f956d4fd2bf51d31910f310d
unknown <fullstackgabe@gmail.com>
2023-01-27 21:00:06 -0300

    desestruturação

--- js/index.js
+++ js/index.js
@@ -1,14 +1,18 @@
 import Controls from "./controls.js";
 import Timer from "./timer.js";
+import { elements } from "./elements.js";
 
-const buttonPlay = document.querySelector(".play");
-const buttonPause = document.querySelector(".pause");
-const buttonStop = document.querySelector(".stop");
-const buttonSet = document.querySelector(".set");
-const buttonSoundOn = document.querySelector(".sound-on");
-const buttonSoundOff = document.querySelector(".sound-off");
-const minutesDisplay = document.querySelector(".minutes");
-const secondsDisplay = document.querySelector(".seconds");
+//desetruturação
+const {
+  buttonPause,
+  buttonPlay,
+  buttonSet,
+  buttonSoundOff,
+  buttonSoundOn,
+  buttonStop,
+  minutesDisplay,
+  secondsDisplay,
+} = elements;
 
 const controls = Controls({
   buttonPause,
